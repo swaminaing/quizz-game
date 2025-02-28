ffaa1b4e0292c1551e239147730bffef89c05f31
swaminaing <smnaing9.smn@gmail.com>
2025-02-28 12:00:35 +0630

    building a basic UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
+import { questions } from "./quizz";
+
+const quizzQuestions = questions;
+
 function App() {
-  return <div>App</div>;
+  return (
+    <main>
+      <h2>Questions 1 / {quizzQuestions.length}</h2>
+      <div className="container">
+        <div className="answers-heading">
+          <h3>{quizzQuestions[0].questionHeader}</h3>
+        </div>
+        <div className="answers-wrapper">
+          {quizzQuestions[0].questionAnswers.map((questionAnswer) => {
+            return <button>{questionAnswer.answer}</button>;
+          })}
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/quizz.js
@@ -0,0 +1,47 @@
+export const questions = [
+    {
+        questionHeader: "What's the capital of France?",
+        questionAnswers: [
+            {answer: "Berlin", isCorrect: false},
+            {answer: "New York", isCorrect: false},
+            {answer: "Paris", isCorrect: true},
+            {answer: "London", isCorrect: false},
+        ]
+    },
+    {
+        questionHeader: "What's the largest planet in our Solar System?",
+        questionAnswers: [
+            {answer: "Earth", isCorrect: false},
+            {answer: "Jupiter", isCorrect: true},
+            {answer: "Mars", isCorrect: false},
+            {answer: "Saturn", isCorrect: false},
+        ]
+    },
+    {
+        questionHeader: "Who wrote 'To Kill a Mockingbird'?",
+        questionAnswers: [
+            {answer: "Harper Lee", isCorrect: true},
+            {answer: "Mark Twain", isCorrect: false},
+            {answer: "Ernest Hemingway", isCorrect: false},
+            {answer: "F. Scott Fitzgerald", isCorrect: false},
+        ]
+    },
+    {
+        questionHeader: "What is the chemical symbol for water?",
+        questionAnswers: [
+            {answer: "H2O", isCorrect: true},
+            {answer: "O2", isCorrect: false},
+            {answer: "CO2", isCorrect: false},
+            {answer: "HO", isCorrect: false},
+        ]
+    },
+    {
+        questionHeader: "What year did the Titanic sink?",
+        questionAnswers: [
+            {answer: "1912", isCorrect: true},
+            {answer: "1905", isCorrect: false},
+            {answer: "1898", isCorrect: false},
+            {answer: "1923", isCorrect: false},
+        ]
+    }
+];
