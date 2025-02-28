3736e52c798d3ac65ce276c0e7900af46096efa8
swaminaing <smnaing9.smn@gmail.com>
2025-02-28 12:28:24 +0630

    created basic quizz app

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,42 @@
 import { useState } from "react";
 import { questions } from "./quizz";
-
+console.log(questions);
 const quizzQuestions = questions;
 
 function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  function handleNextQuestion() {
+    const nextQuestion = currentQuestion + 1;
+    setCurrentQuestion(nextQuestion);
+
+    if (nextQuestion >= quizzQuestions.length) {
+      setCurrentQuestion(0);
+    }
+  }
+
   return (
     <main>
-      <h2>Questions 1 / {quizzQuestions.length}</h2>
+      <h2>
+        Questions {currentQuestion + 1} / {quizzQuestions.length}
+      </h2>
       <div className="container">
         <div className="answers-heading">
-          <h3>{quizzQuestions[0].questionHeader}</h3>
+          <h3>{quizzQuestions[currentQuestion].questionHeader}</h3>
         </div>
         <div className="answers-wrapper">
-          {quizzQuestions[0].questionAnswers.map((questionAnswer) => {
-            return <button>{questionAnswer.answer}</button>;
-          })}
+          {quizzQuestions[currentQuestion].questionAnswers.map(
+            (questionAnswer) => {
+              return (
+                <button
+                  key={questionAnswer.answer}
+                  onClick={handleNextQuestion}
+                >
+                  {questionAnswer.answer}
+                </button>
+              );
+            }
+          )}
         </div>
       </div>
     </main>
