4c744c9b3de1384b9612cf685ee2118b85468768
swaminaing <smnaing9.smn@gmail.com>
2025-02-28 12:54:58 +0630

    implement correct score

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,20 @@ const quizzQuestions = questions;
 
 function App() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [correctScore, setCorrectScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
 
-  function handleNextQuestion() {
+  function handleNextQuestion(isCorrect) {
     const nextQuestion = currentQuestion + 1;
-    setCurrentQuestion(nextQuestion);
 
     if (nextQuestion >= quizzQuestions.length) {
-      setCurrentQuestion(0);
+      setShowScore(true);
+    } else {
+      setCurrentQuestion(nextQuestion);
+    }
+
+    if (isCorrect === true) {
+      setCorrectScore(correctScore + 1);
     }
   }
 
@@ -20,25 +27,31 @@ function App() {
       <h2>
         Questions {currentQuestion + 1} / {quizzQuestions.length}
       </h2>
-      <div className="container">
-        <div className="answers-heading">
-          <h3>{quizzQuestions[currentQuestion].questionHeader}</h3>
+      {showScore ? (
+        <span>
+          Correct score: {correctScore} out of {quizzQuestions.length}
+        </span>
+      ) : (
+        <div className="container">
+          <div className="answers-heading">
+            <h3>{quizzQuestions[currentQuestion].questionHeader}</h3>
+          </div>
+          <div className="answers-wrapper">
+            {quizzQuestions[currentQuestion].questionAnswers.map(
+              (questionAnswer) => {
+                return (
+                  <button
+                    key={questionAnswer.answer}
+                    onClick={() => handleNextQuestion(questionAnswer.isCorrect)}
+                  >
+                    {questionAnswer.answer}
+                  </button>
+                );
+              }
+            )}
+          </div>
         </div>
-        <div className="answers-wrapper">
-          {quizzQuestions[currentQuestion].questionAnswers.map(
-            (questionAnswer) => {
-              return (
-                <button
-                  key={questionAnswer.answer}
-                  onClick={handleNextQuestion}
-                >
-                  {questionAnswer.answer}
-                </button>
-              );
-            }
-          )}
-        </div>
-      </div>
+      )}
     </main>
   );
 }
